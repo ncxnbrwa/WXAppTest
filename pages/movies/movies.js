70c8dee2dd6109bf8698afd914a640fd3eb0db4f
Ixianwen <2864269046@qq.com>
2018-08-07 18:15:15 +0800

    完成电影资讯页

---
+++ pages/movies/movies.js
@@ -0,0 +1,67 @@
+// pages/movies/movies.js
+var app = getApp();
+var util = require('../../utils/util.js')
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    inTheater: {},
+    comingSoon: {},
+    top250: {}
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    var inTheaterUrl = app.globalData.doubanBase + "/v2/movie/in_theaters" + "?start=0&count=3";
+    var comingSoonUrl = app.globalData.doubanBase + "/v2/movie/coming_soon" + "?start=0&count=3";
+    var top250Url = app.globalData.doubanBase + "/v2/movie/top250" + "?start=0&count=3";
+
+    this.getMovieData(inTheaterUrl, "inTheater", "正在热映");
+    this.getMovieData(comingSoonUrl, "comingSoon", "即将上映");
+    this.getMovieData(top250Url, "top250", "电影TOP250");
+
+  },
+
+  getMovieData: function(url, settedKey, slogan) {
+    var that = this;
+    wx.request({
+      url: url,
+      header: {
+        "Content-Type": "application/json"
+      },
+      success: function(res) {
+        console.log(res.data);
+        that.processData(res.data, settedKey, slogan);
+      }
+    })
+  },
+  processData: function(datas, settedKey, slogan) {
+    var movies = [];
+    for (var i in datas.subjects) {
+      var subject = datas.subjects[i];
+      var title = subject.title;
+      if (title.length >= 6) {
+        title = title.substring(0, 6) + "...";
+      }
+      var tmp = {
+        stars: util.convertToStarArray(subject.rating.stars),
+        title: title,
+        average: subject.rating.average,
+        coverageUrl: subject.images.large,
+        movieId: subject.id
+      };
+      movies.push(tmp);
+    }
+    var readyData = {};
+    readyData[settedKey] = {
+      slogan: slogan,
+      movies: movies
+    }
+    this.setData(readyData);
+  }
+})
---
+++ utils/util.js
@@ -0,0 +1,33 @@
+const formatTime = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+}
+
+const formatNumber = n => {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+
+function convertToStarArray(stars) {
+  var num = stars.substring(0, 1);
+  var arr = [];
+  for (var i = 1; i <= 5; i++) {
+    if (i <= num) {
+      arr.push(1);
+    } else {
+      arr.push(0);
+    }
+  }
+  return arr;
+}
+
+module.exports = {
+  formatTime: formatTime,
+  convertToStarArray: convertToStarArray
+}
